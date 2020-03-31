bdb6fcb76a0ec3e9adaff24fc4e6924d1b4fcb8d
Mrtly <mrtly.ca@gmail.com>
2020-03-31 10:13:21 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>mnemonic</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { render } from '@testing-library/react';
+import App from './App';
+
+describe('Testing the starter App with enzyme and jest', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<App />);
+  });
+
+  test('test the word mnemonic with enzyme', () => {
+    expect(wrapper.find('h1').text()).toContain('mnemonic');
+  });
+    
+  test('test mnemonic with the default jest way', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/mnemonic/i);
+    expect(linkElement).toBeInTheDocument();
+  }); 
+});
