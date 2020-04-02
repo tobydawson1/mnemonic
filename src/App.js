2079bddc8d160d008648b7b2fa97a25e13623508
Ovy95 <jackoverton21@hotmail.com>
2020-04-02 10:31:09 +0100

    Added Use effect, Added resizeboard

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect} from 'react'
 
-import Board from './components/board'
+import Board from './components/board/Board'
 
 import initializDeck from './deck'
 
 export default function App() {
   const [cards, setCards]=useState([])
   const [flipped, setFlipped] = useState([])
+  const [dimension, setDimension] = useState(400)
 
   useEffect(() => {
+    resizeBoard()
     setCards(initializDeck())
   }, [])
 
   const handleClick = (id) => setFlipped([...flipped, id])
   
+  const resizeBoard = () => {
+    setDimension(
+      Math.min(
+        document.documentElement.clientWidth,
+        document.documentElement.clientHeight,
+      ),
+    )
+  }
 
   return (
     <div className="App">
