595ca478db3b76b8e14f93bcac9f4b6e3b937f00
Toby Dawson <tobydawson1@gmail.com>
2020-04-02 15:02:14 +0100

    can play game all the way through

--- src/App.js
+++ src/App.js
@@ -17,6 +17,10 @@ export default function App() {
   }, [])
 
   useEffect(() => {
+    preloadImages()
+  }, cards)
+
+  useEffect(() => {
     const resizeListener = window.addEventListener('resize', resizeBoard)
 
     return () => window.removeEventListener('resize', resizeListener)
@@ -37,6 +41,13 @@ export default function App() {
         setTimeout(resetCards, 2000)
       }
     }
+  }
+
+  const preloadImages = () => {
+    cards.map(card => {
+      const src = `/img/${card.type}.svg`
+      new Image().src = src
+    })
   }
 
   const resetCards = () => {
