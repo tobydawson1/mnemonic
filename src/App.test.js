4a977516bfe42d3113a2f04aed55dbd9f3266308
Ovy95 <jackoverton21@hotmail.com>
2020-04-06 22:22:59 +0100

    App test for Animation div

--- src/App.test.js
+++ src/App.test.js
@@ -15,13 +15,23 @@ describe('Testing the starter App with enzyme and jest', () => {
     expect(wrapper.find('h1').text()).toContain('mnemonic');
   });
 
-  test('Test for the h3 tag  enzyme', () => {
+  test('Test for the h2 tag  enzyme', () => {
     expect(wrapper.find('h2').text()).toContain('can you remember where the cards are?');
+  });
+
+  test('Test find the animation div ', () => {
+    expect(wrapper.find('div.animationhere').children()).toHaveLength(0);
   });
     
   test('test mnemonic with the default jest way', () => {
     const { getByText } = render(<App />);
     const linkElement = getByText(/mnemonic/i);
+    expect(linkElement).toBeInTheDocument();
+  }); 
+
+  test('test h2 tag using jest way', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/can you remember where the cards are?/i);
     expect(linkElement).toBeInTheDocument();
   }); 
 
