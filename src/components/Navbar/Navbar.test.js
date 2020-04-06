6d1afe7dad20ccb652dad630f481c45c6af88b25
Ovy95 <jackoverton21@hotmail.com>
2020-04-06 16:43:50 +0100

    Navbar Test :Tests inside wins div for the length

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow, mount } from 'enzyme';
 import { render } from '@testing-library/react';
+import App from '../../App';
 import Navbar from './Navbar';
 
 describe('Testing the starter App with enzyme and jest', () => {
@@ -10,7 +11,22 @@ describe('Testing the starter App with enzyme and jest', () => {
     wrapper = mount(<Navbar />);
   });
 
-  test('test the word mnemonic with enzyme', () => {
+  test('tests to find a button with text with new game', () => {
     expect(wrapper.find('button').text()).toContain('new game');
   });
+
+  test('tests the amount of values inside the wins', () => {
+  expect(wrapper.find('.wins').children()).toHaveLength(3);
+  });
+  
 });
+
+// describe ('Testing for classes using enzyme', ()=> {
+
+
+//   test('tests text with wins:', () => {
+//     const wrap = shallow (<Navbar />);
+//     console.log (wrap.is('.wins'))
+//     expect(wrap.is('wins')).toContain('wins');
+//   });
+// })
