0626fdb5aae3d3c287a58bba08b4da991b114e70
Barri <info@bdesignstudio.co.uk>
2020-04-07 13:57:04 +0100

    Edit ABout text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,12 +7,12 @@ function About() {
       <div className="About">
         <div class="fadebox1">
         <h3> About</h3>
-        <p> This is the Memory card app made by :</p>
+        <p> “This is a team engineering project build as part of weeks 11/12 at Makers academy, link to repository”</p>
         <ul>
-          <li><a href="https://github.com/tobydawson1">Toby</a></li>
-          <li><a href="https://github.com/Mrtly">Myrtle</a></li>
+          <li><a href="https://github.com/tobydawson1/mnemonic">mnemonic repo</a></li>
+          {/* <li><a href="https://github.com/Mrtly">Myrtle</a></li>
           <li><a href="https://github.com/Ovy95">Jack</a></li>
-          <li><a href="https://github.com/Barrif13">Barri</a></li>
+          <li><a href="https://github.com/Barrif13">Barri</a></li> */}
         </ul>
         <Link className="about" to='/'>Back to the game</Link>
       </div>
