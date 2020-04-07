b95262a844c457fb4a08133a15782fb494794ea3
Toby Dawson <tobydawson1@gmail.com>
2020-04-07 10:48:36 +0100

    added difficulty levels

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ export default function App() {
   const [wrongGuesses, setWrongGuesses] = useState(0);
   const [losses, setLosses] = useState(0);
 
+
   useEffect(() => {
     resizeBoard()
   }, [])
@@ -90,13 +91,19 @@ export default function App() {
     setSolved([]);
     setCards(initializeDeck());
     showCards()
-    setTimeout(resetCards, 3000)
     setWrongGuesses(0);
     setScore(0);
   }
 
   const showCards = () => {
-    setFlipped([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+    if (wins<4) {
+      setFlipped([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+      setTimeout(resetCards, 3000)
+    } else if (wins<8) {
+      setFlipped([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+      setTimeout(resetCards, 1500)
+    } else {
+    }
   }
 
   const preloadImages = () => {
