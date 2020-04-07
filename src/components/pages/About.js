be6b5d982241aded3d41b0661aff3e21d373b95c
Barri <info@bdesignstudio.co.uk>
2020-04-07 13:49:24 +0100

    Final css edit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function About (){
+function About() {
   return (
-    <div className="App"> 
-    <h3> About</h3>
-    <p> This is the Memory card app made by :</p>
-    <ul>
-    <li><a href="https://github.com/tobydawson1">Toby</a></li>
-    <li><a href="https://github.com/Mrtly">Myrtle</a></li>
-    <li><a href="https://github.com/Ovy95">Jack</a></li>
-    <li><a href="https://github.com/Barrif13">Barri</a></li>
-    </ul>
-    <Link className="about" to='/'>Back to the game</Link>
+    
+      <div className="About">
+        <div class="fadebox1">
+        <h3> About</h3>
+        <p> This is the Memory card app made by :</p>
+        <ul>
+          <li><a href="https://github.com/tobydawson1">Toby</a></li>
+          <li><a href="https://github.com/Mrtly">Myrtle</a></li>
+          <li><a href="https://github.com/Ovy95">Jack</a></li>
+          <li><a href="https://github.com/Barrif13">Barri</a></li>
+        </ul>
+        <Link className="about" to='/'>Back to the game</Link>
+      </div>
     </div>
   )
 }
-export default  About;
+export default About;
