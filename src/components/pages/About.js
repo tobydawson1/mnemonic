29ba4a778f36051c5199dd4ddf39bfbc52cd1938
Ovy95 <jackoverton21@hotmail.com>
2020-04-07 16:50:20 +0100

    Message about repo added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,6 +14,9 @@ function About() {
         <p>mnemonic is a card game in which all of the cards are laid face down on a surface and two cards are flipped face-up. The object of the game is to turn over all the matching cards, creating a pair. If they're not matching they will flip face down. If they do match they will stay face up and 1 point will be added to the score for matched pairs out of 7.</p>
         <br></br>
         <p>mnemonic also has a progressive feature, which makes the game harder. Once the game starts the cards on the page show for 3 seconds as you progress and get the win tally up, the amount time the cards are shown before hand is descreases. Making the game not as easy. When the reset buttons is push, each of the timings will go back to the defualt settings</p>
+        <br></br>
+        <br></br>
+        <p>Please checkout the repo <p/><p></p>below for more details</p>
         <ul>
           <li><a href="https://github.com/tobydawson1/mnemonic">mnemonic repo</a></li>
         </ul>
