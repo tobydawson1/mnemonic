c153a7a037004e937d1a55c7c1cd4ec14d52130c
Toby Dawson <tobydawson1@gmail.com>
2020-04-07 14:11:42 +0100

    testing again

--- src/App.test.js
+++ src/App.test.js
@@ -45,17 +45,7 @@ describe('Testing the starter App with enzyme and jest', () => {
 describe('Testing the Card', () => {
   let wrapper;
   beforeEach(() => {
-      wrapper = shallow(<Card
-          id={1}
-        
-          width={200}
-          height={200}
-          type={'card_set/back.svg'}
-          flipped={false}
-          solved={false}
-          disabled={false}
-          handleClick={() => handleClick(1)}
-      />)
+    wrapper = mount(<App />);
   });
   it('card is not flipped', () => {
       expect(wrapper.find('.flipped').exists()).toEqual(false);
