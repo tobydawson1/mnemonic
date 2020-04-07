5389bf3770929b2b02b55d076bf701fe2f651ce1
Ovy95 <jackoverton21@hotmail.com>
2020-04-07 16:02:33 +0100

    about Game bio added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,10 @@ function About() {
       <div className="About">
         <div class="fadebox1">
         <h3> About</h3>
-        <p>This is a team engineering project build as part of weeks 11/12 at Makers academy :</p>
+        <p>This is a team engineering project build as part of weeks 11/12 at Makers academy.</p>
+        <p> We were all grouped up and put into our chosen categories we wanted to work in ours was games. So what did we build? </p> 
+        <br></br>
+        <p>mnemonic is a card game in which all of the cards are laid face down on a surface and two cards are flipped face-up. The object of the game is to turn over all the matching cards, creating a pair. If they're not matching they will flip face down. If they do match they will stay face up and 1 point will be added to the score for matched pairs out of 7.</p>
         <ul>
           <li><a href="https://github.com/tobydawson1/mnemonic">mnemonic repo</a></li>
         </ul>
